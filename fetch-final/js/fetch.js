45a7b3ad626e93cdc99b36474e3bcf42bda02a89
Farish Kashefinejad <farishkash@hotmail.com>
2021-03-28 08:52:41 -0700

    fetch starter and fetch final code

---
+++ fetch-final/js/fetch.js
@@ -0,0 +1,28 @@
+// query selector for dynamically generate elements container
+const addressList = document.querySelector("#address-list");
+
+// function to generate address elements
+const generateAddresses = (data) => {
+  // loop over data and generate dynamic element
+  data.addresses.forEach((address) => {
+    // create a div to store our address
+    const newAddressDiv = document.createElement('div');
+    // add tailwind classes
+    newAddressDiv.classList.add("px-6", "pt-4", "pb-2");
+    // create HTML string for each address
+    const addressHTML = `
+    <p>${address.address1}</p>
+    <p>${address.city}</p>
+    <p>${address.state}, ${address.postalCode}</p>
+    `;
+    // set innerHTML of newAddressDrive to addressHTML
+    newAddressDiv.innerHTML = addressHTML;
+    // append newAddressDiv to address-list target
+    addressList.appendChild(newAddressDiv);
+  });
+};
+
+//fetch our data
+fetch("data/data.json")
+.then(response => response.json())
+.then(data => generateAddresses(data));
